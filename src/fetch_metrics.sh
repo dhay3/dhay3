fb677773d92f79b037d76c292bfc57d30d0c7e6f
cyberpelican <62749885+dhay3@users.noreply.github.com>
2024-06-29 23:16:20 +0800

    👷 update ci

--- src/fetch_metrics.sh
+++ src/fetch_metrics.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-
+set -xo
+# https://github.com/vn7n24fzkq/github-profile-summary-cards
 declare -r PROFILE_SUMMARY_CARDS_URL="https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=dhay3&theme=2077"
+# https://github.com/anuraghazra/github-readme-stats
 declare -r README_STATS_URL="https://github-readme-stats.vercel.app/api?username=dhay3&show_icons=true&theme=radical&hide_title=true&card_width=500&hide_border=true"
+# https://github.com/JacobLinCool/LeetCode-Stats-Card
 declare -r LEET_CARD_URL="https://leetcard.jacoblin.cool/dhay3?site=cn&theme=dark"
 
 wget -O assets/profile_summary_card.svg ${PROFILE_SUMMARY_CARDS_URL}
