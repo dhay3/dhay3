0ab59347b5932e68aa0d686d9a2766aa67cf2aaa
cyberpelican <62749885+dhay3@users.noreply.github.com>
2024-06-29 22:46:25 +0800

    💚 (workflows): fix url not found for fetch-svg

--- src/fetch_metrics.sh
+++ src/fetch_metrics.sh
@@ -4,6 +4,6 @@ declare -r PROFILE_SUMMARY_CARDS_URL="https://github-profile-summary-cards.verce
 declare -r README_STATS_URL="https://github-readme-stats.vercel.app/api?username=dhay3&show_icons=true&theme=radical&hide_title=true&card_width=500&hide_border=true"
 declare -r LEET_CARD_URL="https://leetcard.jacoblin.cool/dhay3?site=cn&theme=dark"
 
-wget -O assets/profile_summary_card.svg ${PROFILE_SUMMARY_CARDS}
+wget -O assets/profile_summary_card.svg ${PROFILE_SUMMARY_CARDS_URL}
 wget -O assets/readme_card.svg ${README_STATS_URL}
 wget -O assets/leet_card.svg ${LEET_CARD_URL}
