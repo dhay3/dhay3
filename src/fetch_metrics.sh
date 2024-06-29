d00070edeff77e1b31231a5b53e689bb799bc7b3
cyberpelican <62749885+dhay3@users.noreply.github.com>
2024-06-29 22:38:51 +0800

    👷 (workflows): add fetch-svg workflow

--- src/fetch_metrics.sh
+++ src/fetch_metrics.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-declare -r LEETCODE_METRICS_URL="https://leetcard.jacoblin.cool/dhay3?site=cn&theme=dark"
-declare -r PROFILE_SUMMARY_URL="https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=dhay3&theme=2077"
-declare -r README_STAT_URL="https://github-readme-stats.vercel.app/api?username=dhay3&show_icons=true&theme=radical&hide_title=true&card_width=500&hide_border=true"
+declare -r PROFILE_SUMMARY_CARDS_URL="https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=dhay3&theme=2077"
+declare -r README_STATS_URL="https://github-readme-stats.vercel.app/api?username=dhay3&show_icons=true&theme=radical&hide_title=true&card_width=500&hide_border=true"
+declare -r LEET_CARD_URL="https://leetcard.jacoblin.cool/dhay3?site=cn&theme=dark"
 
-wget ${LEETCODE_METRICS_URL}
+wget -O assets/profile_summary_card.svg ${PROFILE_SUMMARY_CARDS}
+wget -O assets/readme_card.svg ${README_STATS_URL}
+wget -O assets/leet_card.svg ${LEET_CARD_URL}
