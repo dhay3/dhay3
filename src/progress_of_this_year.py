e9605c508e4f5cc5d0caba1f8f7e28a99ef97e19
cyberpelican <62749885+dhay3@users.noreply.github.com>
2024-07-01 22:06:42 +0800

    📝 update profile

--- src/progress_of_this_year.py
+++ src/progress_of_this_year.py
@@ -8,22 +8,22 @@ this_tz = ZoneInfo("Asia/Shanghai")
 start_comment = '<!--START_SECTION:progress-->'
 end_comment = '<!--END_SECTION:progress-->'
 placeholder = fr'{start_comment}[\s\S]+{end_comment}'
+progress_bar_capacity = 50
 
 
-def gen_progress_content() -> str:
-    progress_bar_capacity = 50
+def gen_progress_content(capacity: int, tz: ZoneInfo) -> str:
     seed = uuid.uuid1()
     current_time_of_this_year = datetime.timestamp(datetime.now())
     this_year = datetime.now().year
     start_time_of_this_year = datetime.timestamp(
-        datetime(datetime.now().year, 1, 1, 0, 0, 0, 0, this_tz))
+        datetime(this_year, 1, 1, 0, 0, 0, 0, tz))
     end_time_of_this_year = datetime.timestamp(
-        datetime(datetime.now().year, 12, 31, 23, 59, 59, 0, this_tz))
+        datetime(this_year, 12, 31, 23, 59, 59, 0, tz))
     progress_of_this_year = ((current_time_of_this_year - start_time_of_this_year)
                              / (end_time_of_this_year - start_time_of_this_year))
-    progress_bar = (f'{int(progress_bar_capacity * progress_of_this_year) * "⣿"}'
+    progress_bar = (f'{int(capacity * progress_of_this_year) * "⣿"}'
                     f'{"" if progress_of_this_year == 1 else "⣦"}'
-                    f'{int(progress_bar_capacity - 1 - progress_bar_capacity * progress_of_this_year) * "⣀"}')
+                    f'{int(capacity - 1 - capacity * progress_of_this_year) * "⣀"}')
     progress_content = f"""{start_comment}
 ⌛ Progress of {this_year}
 <!--{seed}-->
@@ -33,7 +33,7 @@ def gen_progress_content() -> str:
 
 
 def gen_readme() -> None:
-    progress_content = gen_progress_content()
+    progress_content = gen_progress_content(progress_bar_capacity, this_tz)
     with open(readme, 'r') as r:
         readme_content = r.read()
     with open(readme, 'w') as w:
